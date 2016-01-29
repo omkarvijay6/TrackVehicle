d7719081431fc50251c70ff3951f6fe3a7c61260
omkarvijay6 <omkarvijay6@gmail.com>
2016-01-29 16:12:59 +0530

    added postgres database settings

--- TrackVehicle/settings.py
+++ TrackVehicle/settings.py
@@ -73,11 +73,15 @@ WSGI_APPLICATION = 'TrackVehicle.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'vehicle_tracker_dev',                      # Or path to database file if using sqlite3.
+        # The following settings are not used with sqlite3:
+        'USER': 'vehicle_tracker',
+        'PASSWORD': 'pAssw0rd',
+        'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
