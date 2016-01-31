5bf779642c3ac814084e748322c279f1d18de056
omkarvijay6 <omkarvijay6@gmail.com>
2016-01-31 09:48:52 +0530

    removed to_representation

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -6,12 +6,3 @@ class VehicleLocationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Location
         fields = ('prev_lat', 'prev_long', 'current_lat', 'current_long',)
-
-
-    def to_representation(self, location_obj):
-        ret = {}
-        ret['previous_latitude'] = location_obj.prev_lat
-        ret['previous_longitude'] = location_obj.prev_long
-        ret['current_latitude'] = location_obj.current_lat
-        ret['current_longitude'] = location_obj.current_long
-        return ret
