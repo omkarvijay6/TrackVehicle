ac5788d16235d1cebe74a33037a949758f3d1c8f
omkarvijay6 <omkarvijay6@gmail.com>
2016-02-02 12:43:18 +0530

    get_or_create location

--- vehicle/services.py
+++ vehicle/services.py
@@ -1,6 +1,7 @@
 from .models import Vehicle, Location
 
 def get_location():
-    vehicle, created = Vehicle.objects.get_or_create(name='TestVehicle')
-    location = Location.objects.first()
+    vehicle, created = Vehicle.objects.get_or_create(
+        name='TestVehicle')
+    location = Location.objects.get_or_create(vehicle=vehicle)
     return location
