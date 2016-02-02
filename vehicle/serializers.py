44cb2b878b6595a8bc5f8bec1d8035a4abfbec55
omkarvijay6 <omkarvijay6@gmail.com>
2016-02-02 12:45:23 +0530

    updated serializer name

--- vehicle/serializers.py
+++ vehicle/serializers.py
@@ -1,15 +1,8 @@
 from rest_framework import serializers
 from vehicle.models import Location
 
-class VehicleLocationSerializer(serializers.ModelSerializer):
+class LocationSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Location
         fields = ('prev_lat', 'prev_long', 'current_lat', 'current_long',)
-
-
-class TestDataSerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model = Location
-		fields = ('prev_lat', 'prev_long', 'current_lat', 'current_long',)
--- vehicle/views.py
+++ vehicle/views.py
@@ -5,24 +5,24 @@ from rest_framework.generics import RetrieveUpdateAPIView, ListCreateAPIView
 
 from vehicle.models import Location
 from vehicle.services import get_location
-from vehicle.serializers import VehicleLocationSerializer, TestDataSerializer
+from vehicle.serializers import LocationSerializer
 # Create your views here.
 
 class VehicleLocation(RetrieveUpdateAPIView):
-	"""
-	Endpoint updates the location of the vehicle in post request
-	Returns the location of the vehicle in get request
-	payload:
-	{
-	    "prev_lat": 123.2,
-	    "prev_long": 12353.3,
-	    "current_lat": 1252.5,
-	    "current_long": 1945.3,
+    """
+    Endpoint updates the location of the vehicle in post request
+    Returns the location of the vehicle in get request
+    payload:
+    {
+        "prev_lat": 123.2,
+        "prev_long": 12353.3,
+        "current_lat": 1252.5,
+        "current_long": 1945.3,
 
-	}
-	prev lat and long used to calculate the distance travelled and velocity of the vehicle
-	"""
-    serializer_class = VehicleLocationSerializer
+    }
+    prev lat and long used to calculate the distance travelled and velocity of the vehicle
+    """
+    serializer_class = LocationSerializer
 
     def get_object(self):
         location = get_location()
@@ -30,11 +30,11 @@ class VehicleLocation(RetrieveUpdateAPIView):
 
 
 class GetTestData(ListCreateAPIView):
-	"""
-	This end point is used to acquire live test data to store in Location model
-	without vehicle.
-	data can be used later to test on Android
-	"""
-	serializer_class = TestDataSerializer
-	queryset = Location.objects.all()
+    """
+    This end point is used to acquire live test data to store in Location model
+    without vehicle.
+    data can be used later to test on Android
+    """
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
 
