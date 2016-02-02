06746f14a061dd2cc8b2b49979a87734ff3693f6
omkarvijay6 <omkarvijay6@gmail.com>
2016-02-02 13:15:22 +0530

    fixed typo

--- vehicle/services.py
+++ vehicle/services.py
@@ -3,5 +3,5 @@ from .models import Vehicle, Location
 def get_location():
     vehicle, created = Vehicle.objects.get_or_create(
         name='TestVehicle')
-    location = Location.objects.get_or_create(vehicle=vehicle)
+    location, created = Location.objects.get_or_create(vehicle=vehicle)
     return location
