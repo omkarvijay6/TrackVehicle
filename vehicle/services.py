35ab44916f7a1b4128ce793b0df6af697998e7c0
omkarvijay6 <omkarvijay6@gmail.com>
2016-02-07 21:18:15 +0530

    fixed issue and added update_test_data for populate testdata file

--- vehicle/services.py
+++ vehicle/services.py
@@ -20,7 +20,7 @@ def update_locations(location_data):
     
     if test_data:
         test_data = json.loads(test_data)
-        count = max(test_data[0].keys())
+        count = max(map(int, test_data[0].keys()))
         test_data[0][int(count) + 1] = location_data
     else:
         test_data = [{1: location_data}, {'location_sequnce': 1}]
@@ -35,3 +35,33 @@ def validate_data(post_data):
         return True
     except:
         return False
+
+
+def update_location_obj(geo_data):
+    """
+    update the current and previous location
+    """
+    location_obj = get_location()
+    data_dict = {'prev_lat': location_obj.current_lat, 'prev_long': location_obj.current_long,
+                 'current_lat': geo_data['lat'], 'current_long': geo_data['long']}
+    for k, v in data_dict.iteritems():
+        setattr(location_obj, k, v)
+    location_obj.save()
+
+
+def update_test_data():
+    """
+    populate the location object and update the testdata json to have a track of current position
+    through location_sequence
+    """
+    with open("testdata.txt") as test_file:
+        test_data = json.loads(test_file.read())
+    location_sequence = test_data[1]["location_sequence"]
+    if int(max(test_data[0].keys())) < location_sequence:
+        test_data[1]["location_sequence"] = 1
+    else:
+        test_data[1]["location_sequence"] = location_sequence + 1
+    geo_data = test_data[0][str(location_sequence)]
+    update_location_obj(geo_data)
+    with open("testdata.txt", "w") as test_file:
+        json.dump(test_data, test_file)
